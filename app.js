7e45ace7b7e9de6fca9624cfba9f4f848f8096da
Lyz03 <lizoe@netcourrier.com>
2021-11-10 11:20:48 +0100

    exo 51

---
+++ app.js
@@ -0,0 +1,9 @@
+let result = 5 + 6;
+let addition = result + 2;
+let soustraction = result - 1;
+
+alert(result * addition);
+alert(result ** soustraction)
+
+let resultModulo = 5 % 2;
+console.log(resultModulo);
